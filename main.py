caacd1902fa431291d639be0a24fc834d06cccb6
Anaël NOUGBODE <alnougbode2@gmail.com>
2025-10-11 02:09:59 +0200

    Launch extract_csv in main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+from etl.extract import extract_csv
+
+from etl.extract import extract_api
+
+#csv_file = r"cyber-etl\data\raw\Financial Data Set.csv"
+
+#df_logs = extract_csv(csv_file)
+
+#if df_logs is not None:
+#    print(df_logs.head())
